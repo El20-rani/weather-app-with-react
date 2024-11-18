b2364619cba21cb5e6799953e4b3f7d658937314
Eleni Zewdu <81524686+El20-rani@users.noreply.github.com>
2024-11-19 00:31:29 +0100

    made a weather search form with react

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Search from "./Search";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <div className="Search">
+      <h1>Weather App</h1>
+      <Search />
+    </div>
   </React.StrictMode>
 );
 
